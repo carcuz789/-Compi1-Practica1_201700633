60f1ad535cfb7c372b5bad911e7eed5674cb874f
carcuz789 <carcuz789@gmail.com>
2020-02-05 20:46:24 -0600

    05/02/2020 8:46

---
+++ src/compi/practica1_201700633/CompiPractica1_201700633.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compi.practica1_201700633;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class CompiPractica1_201700633 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
